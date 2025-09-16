c953235dd6e79aa4b3ded344426465c851743274
Yashraj <yashrajkamble7727@gmail.com>
2025-09-16 17:03:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="justify-center items-center h-screen flex">
+      <h1 className="text-4xl font-bold">
+        BNV WEB DEVELOPMENT TASK BY YASHRAJ KAMBLE
+      </h1>
+    </div>
+  );
+};
+
+export default App;
