946593964328b446c1d6d4ffe5a1898152ab7f41
Yashraj <yashrajkamble7727@gmail.com>
2025-09-16 17:12:21 +0530

    DEV1.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="justify-center items-center h-screen flex">
-      <h1 className="text-4xl font-bold">
+      <h1 className="text-3xl font-bold">
         BNV WEB DEVELOPMENT TASK BY YASHRAJ KAMBLE
       </h1>
     </div>
