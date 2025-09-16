a997686cfa63836a5c6f901cda728b46d76211e2
Yashraj <yashrajkamble7727@gmail.com>
2025-09-17 02:38:07 +0530

    From The Blog section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "../src/components/Header";
 import Nav from "../src/components/Nav";
 import Footer from "../src/components/Footer";
+import BlogSection from "../src/components/BlogSection";
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
+      <BlogSection />
       <Footer />
     </>
   );
