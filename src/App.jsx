956ada3878e643b277902faf4f04699247f8cd0e
Yashraj <yashrajkamble7727@gmail.com>
2025-09-16 20:21:50 +0530

    header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import Header from "../src/components/Header";
 
 const App = () => {
   return (
-    <div className="justify-center items-center h-screen flex">
-      <h1 className="text-3xl font-bold">
-        BNV WEB DEVELOPMENT TASK BY YASHRAJ KAMBLE
-      </h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
