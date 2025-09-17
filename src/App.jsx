9816c467f597b8760859f763d58aa8b9e827dd41
Yashraj <yashrajkamble7727@gmail.com>
2025-09-17 17:14:46 +0530

    ColorSection added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Nav from "../src/components/Nav";
 import Footer from "../src/components/Footer";
 import BlogSection from "../src/components/BlogSection";
 import Feedback from "../src/components/Feedback";
+import ColorSection from "../src/components/ColorSection";
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
+      <ColorSection />
       <Feedback />
       <BlogSection />
       <Footer />
